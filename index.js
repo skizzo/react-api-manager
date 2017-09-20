fc8d336f0660ab637f0992d15d8ff01c560b2ecc
Stephan Müller <steff@nerdy-birdy.com>
2017-09-21 01:32:00 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+'use strict';
+
+/**
+ * Adds commas to a number
+ * @param {number} number
+ * @param {string} locale
+ * @return {string}
+ */
+module.exports = function(number, locale) {
+    return number.toLocaleString(locale);
+};
 console.log ("react-api-manager included successfully.")
